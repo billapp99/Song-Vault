b43e9bf158df778e2fae57f1a0d824fe84c9da7c
billapp99 <bappiagyei62@gmail.com>
2024-09-29 00:28:34 -0400

    Added Components

---
+++ src/main/resources/webapp/SongWebApp/src/app/song-list-component/song-list-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-list-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './song-list-component.component.html',
+  styleUrl: './song-list-component.component.css'
+})
+export class SongListComponentComponent {
+
+}
